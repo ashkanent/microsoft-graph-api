f67ae4d3b94647d106fcd7a9f0752410f747d1b5
aentezari <aentezari@venacorp.com>
2020-02-25 14:47:23 -0500

    basic excel functionalities

--- src/main/java/com/ashkan/graphapi/controller/controller.java
+++ src/main/java/com/ashkan/graphapi/controller/controller.java
@@ -2,7 +2,9 @@ package com.ashkan.graphapi.controller;
 
 import com.ashkan.graphapi.auth.Authentication;
 import com.ashkan.graphapi.auth.GraphUser;
+import com.ashkan.graphapi.services.Excel;
 import com.ashkan.graphapi.services.OneDrive;
+import com.microsoft.graph.models.extensions.DriveItem;
 import com.microsoft.graph.models.extensions.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -27,6 +29,12 @@ public class controller {
 		System.out.println("Welcome " + user.displayName);
 //		OneDrive.listAllFiles(accessToken);
 		final String testFileId = "01AXVMZA3U5XQPAI3WABALMGDW2V3HU23Z";
+		final String testSheetId = "{00000000-0001-0000-0000-000000000000}";
+		DriveItem testFile = OneDrive.getFileById(accessToken, testFileId);
+//		Excel.createNewWorksheetForWorkbook(accessToken, testFile, "Ashkan");
+		Excel.printWorksheetsForWorkbook(accessToken, testFile);
+		String cellValue = Excel.getCellValueForWorkbookByRowAndColumn(accessToken, testFile, testSheetId, 0, 0);
+		System.out.println("Cell value is: " + cellValue);
 
 	}
 }
---
+++ src/main/java/com/ashkan/graphapi/services/Excel.java
@@ -0,0 +1,63 @@
+package com.ashkan.graphapi.services;
+
+import com.ashkan.graphapi.auth.GraphUser;
+import com.google.gson.JsonArray;
+import com.microsoft.graph.core.ClientException;
+import com.microsoft.graph.models.extensions.DriveItem;
+import com.microsoft.graph.models.extensions.IGraphServiceClient;
+import com.microsoft.graph.models.extensions.WorkbookRange;
+import com.microsoft.graph.models.extensions.WorkbookSessionInfo;
+import com.microsoft.graph.models.extensions.WorkbookWorksheet;
+import com.microsoft.graph.requests.extensions.IWorkbookWorksheetCollectionPage;
+import com.microsoft.graph.requests.extensions.WorkbookWorksheetCollectionPage;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class Excel {
+
+	public static String createSession(String accessToken, String fileId) {
+		IGraphServiceClient graphClient = GraphUser.ensureGraphClient(accessToken);
+
+		WorkbookSessionInfo sessionInfo = graphClient.me().drive().items(fileId).workbook().createSession(true).buildRequest().post();
+
+		return sessionInfo.id;
+	}
+
+	public static void printWorksheetsForWorkbook(String accessToken, DriveItem givenFile) {
+		IGraphServiceClient graphClient = GraphUser.ensureGraphClient(accessToken);
+
+		IWorkbookWorksheetCollectionPage worksheets = graphClient.me().drive().items(givenFile.id).workbook().worksheets().buildRequest().get();
+		for (WorkbookWorksheet worksheet : worksheets.getCurrentPage()) {
+			System.out.println(worksheet.name + " (" + worksheet.id + ")");
+		}
+	}
+
+	public static void createNewWorksheetForWorkbook(String accessToken, DriveItem givenFile, String sheetName) {
+		IGraphServiceClient graphClient = GraphUser.ensureGraphClient(accessToken);
+
+		WorkbookWorksheet newWorkbookWorksheet = new WorkbookWorksheet();
+		newWorkbookWorksheet.name = sheetName;
+		graphClient.me().drive().items(givenFile.id).workbook().worksheets().buildRequest().post(newWorkbookWorksheet);
+	}
+
+	public static WorkbookWorksheet getWorksheetFromWorkbookById(String accessToken, DriveItem givenFile, String givenId) {
+		IGraphServiceClient graphClient = GraphUser.ensureGraphClient(accessToken);
+
+		return graphClient.me().drive().items(givenFile.id).workbook().worksheets(givenId).buildRequest().get();
+	}
+
+	public static String getCellValueForWorkbookByRowAndColumn(String accessToken, DriveItem givenFile, String sheetId, Integer rowIndex, Integer columnIndex) {
+		IGraphServiceClient graphClient = GraphUser.ensureGraphClient(accessToken);
+		createSession(accessToken, givenFile.id);
+		WorkbookRange range = new WorkbookRange();
+
+		try {
+			range = graphClient.me().drive().items(givenFile.id).workbook().worksheets(URLEncoder.encode(sheetId, StandardCharsets.UTF_8.toString())).cell(rowIndex, columnIndex).buildRequest().get();
+		} catch (ClientException | UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return ((JsonArray)((JsonArray)range.values).get(0)).get(0).toString();
+	}
+}
